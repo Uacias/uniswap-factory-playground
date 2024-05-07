03c7b17f439f979f420d61c9f2af2a648d7aba72
Uacias <uaciasdev@gmail.com>
2024-05-07 11:00:28 +0200

    add deactivate do deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,5 @@ forge create \
     --rpc-url $RPC_URL \
     --private-key $PRIVATE_KEY \
     src/UniswapFactory.sol:UniswapV2Factory \
-    --constructor-args $FEE_TO_SETTER 
+    --constructor-args $FEE_TO_SETTER && \
+deactivate
